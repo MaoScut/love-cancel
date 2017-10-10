684089b32f13247e98eb2f8feb03088bab0c726c
MaoScut <849345372@qq.com>
2017-10-10 17:39:34 +0800

    改配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,41 @@
-var { resolve } = require('path');
-var webpack = require('webpack');
+const { resolve } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: [
+  entry: [
     'react-hot-loader/patch',
     // activate HMR for React
-
-    'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
-    './app/index.jsx'
+    './app/index.jsx',
     // the entry point of our app
   ],
-	output: {
-		path: resolve(__dirname, 'build'),
-		filename: 'bundle.js'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		historyApiFallback: true,
-		inline: true,
-		hot: true,
-	},
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    // historyApiFallback: true,
+    inline: true,
+    hot: true,
+  },
 
-	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			enforce: "pre",
-			loader: "babel-loader",
-			include: /node_modules/,
-		},
-		{
-			test: /\.(js|jsx)$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/,
-		}, 
-		{
-			test: /\.scss$/,
-			use: ["style-loader", "css-loader", "sass-loader"],
-			exclude: /node_modules/,
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: 'babel-loader',
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      exclude: /node_modules/,
 
-		}]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin(),
-	],
-	resolve: {
-		extensions: ['.js', 'jsx']
-	},
-}
+    }],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
